e3c940cd66eb2ff641d34a6f354891bf9e2956e2
Noronha <inc.691@gmail.com>
2020-07-25 09:37:29 -0300

    Token discord guardado em variável de ambiente

--- Bot.js
+++ Bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const config = require('./config.js');
 
 client.on('ready', () => {
     client.user.setActivity("Bot Online!")
