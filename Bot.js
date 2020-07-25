6364910220845cf7a959d13f208019fae561ac1d
Noronha <inc.691@gmail.com>
2020-07-25 10:58:06 -0300

    Adiciona funcionalidade  para mostrar os champions grátis da semana do league of legends

--- Bot.js
+++ Bot.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const axios = require('axios').default;
+
 const client = new Discord.Client();
 const config = require('./config.js');
 
@@ -39,6 +41,26 @@ client.on('message', async msg => {
 
     if ( comando === 'mauro') {
         msg.channel.send("", {files: ['./assets/mauro.jpg']});
+    }
+
+    if ( comando === 'lfw' ) {
+        const responseRiot = await axios.get('https://br1.api.riotgames.com/lol/platform/v3/champion-rotations', {headers: {
+            'X-Riot-Token': process.env.riot_token
+        }});
+
+        const championIds = responseRiot.data.freeChampionIds;
+
+        const allChampionsResponse = await axios.get('http://ddragon.leagueoflegends.com/cdn/10.15.1/data/en_US/champion.json');
+
+        const allChampions = allChampionsResponse.data.data;
+
+        const championNames = Object.values(allChampions)
+            .filter(champ => championIds.includes(parseInt(champ.key)))
+            .map(champ => champ.name);
+
+        championNames.forEach(name => {
+            msg.channel.send(`${championNames.indexOf(name) + 1}° Champion da free weak essa semana é ${name}`);
+        });
     }
 })
 
